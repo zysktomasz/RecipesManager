6103925455f2572af6d9cc43ffe0c82cb01f679d
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-09 12:56:59 +0200

    add POCO models (Ingredient, Recipe)

---
+++ RM.Data/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Data.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Descripton { get; set; } // np "250g macaroni"
+        public Recipe Recipe { get; set; } // recipe that this Ingredient corresponds to
+    }
+}
---
+++ RM.Data/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Data.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
