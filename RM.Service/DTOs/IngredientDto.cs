e24e553b7b40b3f40ffe842edf518be883cb921d
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-11 20:25:15 +0200

    add Ingredient, RecipeWithIngredients DTOs

---
+++ RM.Service/DTOs/IngredientDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Service.DTOs
+{
+    public class IngredientDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Unit { get; set; }
+    }
+}
---
+++ RM.Service/DTOs/RecipeWithIngredientsDto.cs
@@ -0,0 +1,20 @@
+﻿using RM.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Service.DTOs
+{
+    public class RecipeWithIngredientsDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<IngredientDto> Ingredients { get; set; }
+
+        public RecipeWithIngredientsDto()
+        {
+            Ingredients = new List<IngredientDto>();
+        }
+    }
+}
