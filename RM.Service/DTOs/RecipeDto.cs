d8a81f6e736ad3bdc69d29e159a484abf363fa48
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-11 23:10:39 +0200

    add RecipeDto

---
+++ RM.Service/DTOs/RecipeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Service.DTOs
+{
+    public class RecipeDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
