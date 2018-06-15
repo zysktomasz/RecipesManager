daa4ad3cec373295f494bb9feb926002fd4b711a
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-15 15:53:42 +0200

    add Ingredient Repository interface & implementation

---
+++ RM.Repo/Interfaces/IIngredientRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Repo.Interfaces
+{
+    public interface IIngredientRepository
+    {
+
+    }
+}
---
+++ RM.Repo/Repositories/IngredientRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RM.Data.Models;
+using RM.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Repo.Repositories
+{
+    public class IngredientRepository : Repository<Ingredient>, IIngredientRepository
+    {
+        private ApplicationContext context { get; }
+
+        public IngredientRepository(DbContext context) 
+            : base(context as ApplicationContext)
+        {
+        }
+    }
+}
