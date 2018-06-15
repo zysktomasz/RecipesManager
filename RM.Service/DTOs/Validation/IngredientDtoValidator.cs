d0e9ca97c5df2978bdb22b89e1c2a87d70afdb47
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-15 15:46:43 +0200

    add FluentValidation for IngredientDto and IngredientsDto collection

---
+++ RM.Service/DTOs/Validation/IngredientDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RM.Service.DTOs.Validation
+{
+    public class IngredientDtoValidator : AbstractValidator<IngredientDto>
+    {
+        public IngredientDtoValidator()
+        {
+            RuleFor(i => i.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+            RuleFor(i => i.Value)
+                .NotEmpty()
+                .GreaterThan(0);
+            RuleFor(i => i.Unit)
+                .NotEmpty()
+                .MaximumLength(30);
+        }
+    }
+}
--- RM.Service/DTOs/Validation/RecipeWithIngredientsDtoValidator.cs
+++ RM.Service/DTOs/Validation/RecipeWithIngredientsDtoValidator.cs
@@ -6,10 +6,15 @@ namespace RM.Service.DTOs.Validation
     {
         public RecipeWithIngredientsDtoValidator()
         {
+            // Recipe properties validation
             RuleFor(recipe => recipe.Title)
                 .NotEmpty();
             RuleFor(recipe => recipe.Description)
                 .NotEmpty();
+
+            // Recipe.Ingredients collection validation
+            RuleFor(recipe => recipe.Ingredients)
+                .SetCollectionValidator(new IngredientDtoValidator());
         }
     }
 }
