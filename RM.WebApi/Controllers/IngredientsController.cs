1086e20c2f9371392e669aae33d56dc02469f844
zysktomasz <zysk.tomasz95@gmail.com>
2018-06-15 19:16:15 +0200

    add IngredientController PUT method

--- RM.WebApi/Controllers/IngredientsController.cs
+++ RM.WebApi/Controllers/IngredientsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RM.Service.DTOs;
+using RM.Service.Interfaces;
 
 namespace RM.WebApi.Controllers
 {
@@ -11,6 +13,25 @@ namespace RM.WebApi.Controllers
     [ApiController]
     public class IngredientsController : ControllerBase
     {
+        private readonly IIngredientService _ingredientService;
 
+        public IngredientsController(IIngredientService ingredientService)
+        {
+            _ingredientService = ingredientService;
+        }
+
+        [HttpPut("{ingredientId}")]
+        public IActionResult UpdateIngredient(int ingredientId, [FromBody] IngredientDto modifiedIngredient)
+        {
+            // TODO: find a way to avoid having to load ingredient twice
+            var ingredientToUpdate = _ingredientService.GetIngredientById(ingredientId);
+
+            if (ingredientToUpdate == null)
+                return NotFound();
+
+            _ingredientService.UpdateIngredient(ingredientId, modifiedIngredient);
+
+            return NoContent();
+        }
     }
 }
